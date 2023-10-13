5f78e851be77f8223d3974962cf7205e63b61e23
Sharad Satsangi <>
2023-10-13 15:02:15 +0000

    Super Hot- Fix

--- test/test-server.js
+++ test/test-server.js
@@ -27,7 +27,7 @@ describe('GET /', function () {
         if (err) {
           return done(err);
         }
-        chai.assert.isTrue(res.text.includes("<h1>Welcome to Acela</h1>"));
+        chai.assert.isTrue(res.text.includes("Welcome to Acela"));
         return done();
       });
 
